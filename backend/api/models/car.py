1a9036910e9e299af41e43d593fc363b09084b99
VelesW <michalwilk296@gmail.com>
2025-01-18 15:03:31 +0100

    Add models

---
+++ backend/api/models/car.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Car(models.Model):
+    available = models.BooleanField()
+    manufacturer = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    petrol_type = models.CharField(max_length=20, choices=[('petrol', 'Petrol'), ('diesel', 'Diesel'), ('electric', 'Electric')])
+    drive_type = models.CharField(max_length=20, choices=[('fwd', 'FWD'), ('rwd', 'RWD'), ('awd', 'AWD')])
+    segment = models.CharField(max_length=20, choices=['A', 'B', 'C', 'D', 'E', 'F', 'J', 'M', 'S'])
+    engine_size = models.CharField(max_length=10)
+    horse_power = models.IntegerField()
+    price_per_day = models.DecimalField(max_digits=8, decimal_places=2)
+
+def __str__(self) -> str:
+    return f"{self.year} {self.make} {self.model} - {self.segment}"
